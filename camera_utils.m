5871d897d13e11d518c8069c5a0ff3cee2c12474
Berenice02 <foderaro.elisa@gmail.com>
2024-04-02 18:25:58 +0200

    Change names and add comments

--- camera_utils.m
+++ camera_utils.m
@@ -1,13 +1,21 @@
 1;
 
+% find the position in the image of an observed point 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% camera_infos: is an object containing the informations about the camera:
+%%               [K, T, z_near, z_far, width, height]
+%% point:        is a 3x1 vector containing the position of the point wrt the world frame
+%% robot_pose:   is a 3x3 transform matrix representing the pose of the robot
+%%
+%% p_img:        is a 2x1 vector containg the [col;row] of the point in the image
 function p_img = project_point(camera_infos, point, robot_pose)
     % Initialization as an out-of-image point
-    p_img = [-1 -1];
+    p_img = [-1; -1];
 
     % World to camera transform
     world_to_camera = robot_pose*camera_infos.T;
     % Project the point
-    point_in_camera = world_to_camera(1:3, 1:3)'*(point'-world_to_camera(1:3, 4));
+    point_in_camera = world_to_camera(1:3, 1:3)'*(point-world_to_camera(1:3, 4));
 
     % Ignore it if it's too close or too far
     if ( point_in_camera(3) < camera_infos.z_near ||
@@ -25,3 +33,4 @@ function p_img = project_point(camera_infos, point, robot_pose)
         return;
     end
 end
+
--- geometry_helpers.m
+++ geometry_helpers.m
@@ -3,17 +3,6 @@
 % the lectures of probabilistic robotics at Sapienza, University of Rome.
 % Copyright (c) 2016 Bartolomeo Della Corte, Giorgio Grisetti
 
-
-function T=compute_odometry_trajectory(U)
-	T=zeros(size(U,1),3);
-	current_T=v2t(zeros(1,3));
-	for i=1:size(U,1),
-		u=U(i,1:3)';
-		current_T*=v2t(u);
-		T(i,1:3)=t2v(current_T)';
-	end
-end
-
 % computes the pose 2d pose vector v from an homogeneous transform A
 % A:[ R t ] 3x3 homogeneous transformation matrix, r translation vector
 % v: [x,y,theta]  2D pose vector
@@ -33,7 +22,6 @@ function A=v2t(v)
 	0   0  1  ];
 end
 
-
 % normalizes and angle between -pi and pi
 % th: input angle
 % o: output angle
@@ -51,24 +39,6 @@ endfunction
 % the lectures of probabilistic robotics at Sapienza, University of Rome.
 % Copyright (c) 2016 Bartolomeo Della Corte, Giorgio Grisetti
 
-#rotation matrix around x axis
-function R=Rx(rot_x)
- c=cos(rot_x);
- s=sin(rot_x);
- R= [1  0  0;
-     0  c  -s;
-     0  s  c];
-endfunction
-
-#rotation matrix around y axis
-function R=Ry(rot_y)
- c=cos(rot_y);
- s=sin(rot_y);
- R= [c  0  s;
-     0  1  0;
-     -s  0 c];
-endfunction
-
 #rotation matrix around z axis
 function R=Rz(rot_z)
  c=cos(rot_z);
@@ -78,20 +48,11 @@ function R=Rz(rot_z)
       0  0  1];
 endfunction
 
-function R=angles2R(a)
-  R=Rx(a(1))*Ry(a(2))*Rz(a(3));
-endfunction;
-
-#from 6d vector to homogeneous matrix
-function T=v2t3d(v)
-    T=eye(4);
-    T(1:3,1:3)=angles2R(v(4:6));
-    T(1:3,4)=v(1:3);
-endfunction;
-
 % Custom
 #from 3d vector on 2d to homogeneous matrix on 3d
-function T = from_2d_to_3d(v)
+function T = from_3dv_to_3dt(v)
 	v = [v(1:2)'; 0; 0; 0; v(3)];
-	T = v2t3d(v);
+  T = eye(4);
+  T(1:3,1:3) = Rz(v(3));
+	T(1:3,4)=[v(1:2); 0];
 end
