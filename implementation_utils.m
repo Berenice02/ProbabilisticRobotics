d3228d4782432236fbe3b7ebdeb4295cdf637a4d
Berenice02 <foderaro.elisa@gmail.com>
2024-04-03 15:56:31 +0200

    Implemented boxPlus and getIndex functions

--- implementation_utils.m
+++ implementation_utils.m
@@ -8,45 +8,43 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% XR:  is a 3x3xnum_poses array of homogeneous transform matrices
 %% XL:  is a 3xnum_landmarks array of 3D positions
-%% dx:  the perturbation vector of appropriate dimensions
+%% dx:  num_poses x 3 + num_landmarks x 3 perturbation vector
 %%
 %% XR:  XR after applying the perturbation
 %% XL:  XL after applying the perturbation
 function [XR, XL]=boxPlus(XR, XL, dx)
   global num_poses, num_landmarks;
-
+  % Robot poses
+  for i=1:num_poses
+    index_of_perturbation = getPoseIndex(i);
+    dxr = dx(index_of_perturbation:index_of_perturbation+2);
+    XR(:, :, i) = v2t(dxr) * XR(:, :, i);
+  end
+  % Landmark positions
+  for i=1:num_landmarks
+    index_of_perturbation = getLandmarkIndex(i);
+    dxl = dx(index_of_perturbation:index_of_perturbation+2);
+    XL(:, :, i) += dxl;
+  end
 end
 
-% transform a long perturbation vector into two matrices
+% get the index of the perturbation vector corresponding to the index of the pose
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% dx: num_poses x 3 + num_landmarks x 3 perturbation vector
+%% pose_index: the index of the pose for which we want the index
 %%
-%% xr: 3xnum_poses matrix of robot poses perturbations
-%% xl: 3xnum_landmarks matrix of landmark positions perturbations
-function [xr, xl]= v2mPerturbation(dx)
+%% idx: the index of the pose
+function idx = getPoseIndex(pose_index)
     global num_poses, num_landmarks;
-    xr = zeros(3, num_poses);
-    xl = zeros(3, num_landmarks);
-
-    
-
+    idx = ((pose_index-1) * 3) + 1;
 end
 
-% transform two perturbation matrices into a long vector
+% get the index of the perturbation vector corresponding to the index of the landmark
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% xr: 3xnum_poses matrix of robot poses perturbations
-%% xl: 3xnum_landmarks matrix of landmark positions perturbations
+%% landmark_index: the index of the landmark for which we want the index
 %%
-%% dx: num_poses x 3 + num_landmarks x 3 perturbation vector
-function dx= m2vPerturbation(xr, xl)
+%% idx: the index of the landmark
+function idx = getLandmarkIndex(landmark_index)
     global num_poses, num_landmarks;
-    dx = zeros(num_poses*3+num_landmarks*3, 1);
-    for i=1:num_poses
-        dx(i) = xr(:, i)';
-    end
-
-    for i=1:num_landmarks
-        dx(i+num_poses*3) = xl(:, i)';
-    end
-
+    idx = (num_poses * 3) + ((landmark_index - 1) * 3) + 1;
 end
+
