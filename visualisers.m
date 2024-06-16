2aafec5fb9b82649714e3a491cf59b8e37726268
Berenice02 <foderaro.elisa@gmail.com>
2024-06-16 16:45:02 +0200

    Edit visualization functions and save images

--- visualisers.m
+++ visualisers.m
@@ -5,89 +5,149 @@ COLORS.GREEN = "#65e24b";
 COLORS.BLUE = "#4b65e2";
 COLORS.YELLOW = "#eace9d";
 
-function _ = plot_trajectories(traj)
+function _ = plot_trajectories(XR, initial_odom, gt_poses)
     global COLORS
-    % Plot the trajectory from the odometry poses
-    plot(traj(:,1),traj(:,2), 'color', COLORS.PURPLE, 'linewidth', 2);
-    hold on;
-
-    % Plot the trajectory from the ground truth poses
-    plot(traj(:,4),traj(:,5), 'color', COLORS.GREEN, 'linewidth', 2);
-    hold off;
-
-    % TODO add optimized
-    legend('odometry','ground truth')
-end
-
-function _ = plot_landmarks(gt_l)
-    global COLORS
-    plot3(gt_l(:,1),gt_l(:,2),gt_l(:,3), '+', 'color', COLORS.GREEN);
-    hold on;
-    % plot3(XL_guess(:,1),XL_guess(:,2),XL_guess(:,3),'o', 'color', COLORS.PURPLE,"linewidth",2);
-
-    %z= zeros(200,1);
-    %plot3(traj(:,4),traj(:,5),z, '-', 'color', COLORS.PURPLE);
-
-    % TODO add guess
-    % TODO add optimized
-    hold off;
-
-    legend('ground truth');
-end
-
-function _ = plot_chi_stats()
-    % subplot(3,2,1);
-    % plot(chi_stats_r, 'r-', "linewidth", 2);
-    % legend("Chi Poses"); grid; xlabel("iterations");
-    % subplot(3,2,2);
-    % plot(num_inliers_r, 'b-', "linewidth", 2);
-    % legend("#inliers"); grid; xlabel("iterations");
-
-    % subplot(3,2,3);
-    % plot(chi_stats_l, 'r-', "linewidth", 2);
-    % legend("Chi Landmark"); grid; xlabel("iterations");
-    % subplot(3,2,4);
-    % plot(num_inliers_l, 'b-', "linewidth", 2);
-    % legend("#inliers"); grid; xlabel("iterations");
-
-    % subplot(3,2,5);
-    % plot(chi_stats_p, 'r-', "linewidth", 2);
-    % legend("Chi Proj"); grid; xlabel("iterations");
-    % subplot(3,2,6);
-    % plot(num_inliers_p, 'b-', "linewidth", 2);
-    % legend("#inliers");grid; xlabel("iterations");
-end
-
-function _ = plot_H()
-    % H_ =  H./H;                      # NaN and 1 element
-    % H_(isnan(H_))=0;                 # Nan to Zero
-    % H_ = abs(ones(size(H_)) - H_);   # switch zero and one
-    % H_ = flipud(H_);                 # switch rows
-    % colormap(gray(64));
-    % hold on;
-    % image([0.5, size(H_,2)-0.5], [0.5, size(H_,1)-0.5], H_*64);
-    % hold off;
-end
-
-function _ = plot_state(traj, world_landmarks)
-    % 2D Trajectories
     h = figure(1);
-    plot_trajectories(traj)
-    grid;
+    
+    subplot(1, 2, 1);
+    % Plot the trajectory from the ground truth poses
+    plot(gt_poses(:, 1),gt_poses(:, 2), 'color', COLORS.GREEN, 'linewidth', 2);
+    hold on;
+    % Plot the trajectory from the odometry poses
+    plot(initial_odom(:,1), initial_odom(:,2), 'color', COLORS.PURPLE, 'linewidth', 2);
+    grid
+    daspect([1 1 1])
+    title('Initial data')
+    legend('ground truth', 'odometry')
+    
+    subplot(1, 2, 2);
+    % Plot the trajectory from the ground truth poses
+    plot(gt_poses(:, 1),gt_poses(:, 2), 'color', COLORS.GREEN, 'linewidth', 2);
+    hold on;
+    % Plot the trajectory from the optimized poses
+    plot(XR(:, 1),XR(:, 2), 'color', COLORS.BLUE, 'linewidth', 2);
+    grid
+    daspect([1 1 1])
+    title('After optimization')
+    legend('ground truth', 'optimized')
 
-    % 3D Landmark Positions
+    %waitfor(h);
+end
+
+function _ = plot_landmarks(XL, initial_guess, gt_landmarks)
+    global COLORS
     h = figure(2);
-    plot_landmarks(world_landmarks)
-    grid;
 
-    % Chi Evolution
+    subplot(2, 2, 1);
+    % Plot the ground truth landmarks
+    plot3(gt_landmarks(1, :),gt_landmarks(2, :),gt_landmarks(3, :), '+', 'color', COLORS.GREEN);
+    hold on;
+    % Plot the initial guess of landmarks
+    plot3(initial_guess(1, :),initial_guess(2, :),initial_guess(3, :),'o', 'color', COLORS.PURPLE);
+    grid
+    daspect([1 1 1])
+    title('Initial data')
+    legend('ground truth', 'initial guess')
+    
+    subplot(2, 2, 2);
+    % Plot the ground truth landmarks
+    plot3(gt_landmarks(1, :),gt_landmarks(2, :),gt_landmarks(3, :), '+', 'color', COLORS.GREEN);
+    hold on;
+    % Plot the optimized landmarks
+    plot3(XL(1, :),XL(2, :),XL(3, :),'o', 'color', COLORS.BLUE);
+    grid
+    daspect([1 1 1])
+    title('After optimization')
+    legend('ground truth', 'optimized')
+
+    subplot(2, 2, 3);
+    % Plot the ground truth landmarks
+    plot3(gt_landmarks(1, :),gt_landmarks(2, :),gt_landmarks(3, :), '+', 'color', COLORS.GREEN);
+    hold on;
+    % Plot the initial guess of landmarks
+    plot3(initial_guess(1, :),initial_guess(2, :),initial_guess(3, :),'o', 'color', COLORS.PURPLE,"linewidth",2);
+    grid
+    view(-32, 17)
+    xlim([-10, 10])
+    ylim([-10, 10])
+    zlim([0, 2])
+    title('Close up')
+
+    subplot(2, 2, 4);
+    % Plot the ground truth landmarks
+    plot3(gt_landmarks(1, :),gt_landmarks(2, :),gt_landmarks(3, :), '+', 'color', COLORS.GREEN);
+    hold on;
+    % Plot the optimized landmarks
+    plot3(XL(1, :),XL(2, :),XL(3, :),'o', 'color', COLORS.BLUE,"linewidth",2);
+    grid
+    view(-32, 17)
+    xlim([-10, 10])
+    ylim([-10, 10])
+    zlim([0, 2])
+    title('Close up')
+
+    %waitfor(h);
+end
+
+function _ = plot_state(XR, initial_odom, gt_poses, XL, initial_guess, gt_landmarks)
+    % 2D Trajectories
+    plot_trajectories(XR, initial_odom, gt_poses)
+    
+    % % 3D Landmark Positions
+    plot_landmarks(XL, initial_guess, gt_landmarks)
+end
+
+function _ = plot_statistics(chi_stats, inliers_stats)
+    global COLORS
+
     h = figure(3);
-    plot_chi_stats()
-    grid;
 
-    % H Matrix
+    % Chi Evolution    
+    subplot(2, 2, 1)
+    plot(chi_stats(1, :), 'color', COLORS.RED, 'linewidth', 2);
+    title('chi evolution')
+    grid
+    legend("Chi Landmark")
+
+    subplot(2, 2, 3)
+    plot(chi_stats(2, :), 'color', COLORS.RED, 'linewidth', 2);
+    grid
+    legend("Chi Poses")
+    xlabel("iterations")
+
+    % Inliers Evolution 
+    subplot(2, 2, 2)
+    plot(inliers_stats(1, :), 'color', COLORS.BLUE, 'linewidth', 2);
+    title('inliers evolution')
+    grid
+    legend("#inliers Landmark", 'location', 'southeast')
+
+    subplot(2, 2, 4)
+    plot(inliers_stats(2, :), 'color', COLORS.BLUE, 'linewidth', 2);
+    grid
+    legend("#inliers Poses")
+    xlabel("iterations")
+
+    waitfor(h);
+end
+
+function _ = plot_RMSE(rmse_stats)
+    % RMSE    
     h = figure(4);
-    plot_H()
+    subplot(1, 3, 1)
+    plot(rmse_stats(1, :), 'color', COLORS.YELLOW, 'linewidth', 2);
+    xticks([])
+    title('RMSE poses. Rotation')
+
+    subplot(1, 3, 2)
+    plot(rmse_stats(2, :), 'color', COLORS.YELLOW, 'linewidth', 2);
+    xticks([])
+    title('RMSE poses. Translation')
+
+    subplot(1, 3, 3)
+    plot(rmse_stats(3, :), 'color', COLORS.YELLOW, 'linewidth', 2);
+    xticks([])
+    title('RMSE landmarks')
 
     waitfor(h);
 end
